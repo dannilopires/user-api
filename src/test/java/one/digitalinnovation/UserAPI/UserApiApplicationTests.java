98a2a53912bdb9cd0133842cc3024e9f67b2fe74
dannilo <dannilo-pires@hotmail.com>
2021-06-24 15:11:48 -0300

    initial commit - setting up basic configurations

---
+++ src/test/java/one/digitalinnovation/UserAPI/UserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.UserAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
